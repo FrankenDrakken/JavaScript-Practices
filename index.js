0ccf78ca2c6a00be6d2d101e1b9d7d522c8788e7
Franco_Ramos <134591963+FrankenDrakken@users.noreply.github.com>
2023-10-06 13:47:14 -0400

    Added round borders and started JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-let counter = document.getElementById(counter)
+let counter = document.getElementById(counter1)
+let NCR = document.getElementById(NCR-counter)
+let count = 0
+document.getElementById("NCR-counter").textContent = count
+
+function countNCR() {
+    count += 1
+    NCR.textContent = count
+}
+
